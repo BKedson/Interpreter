265a774a59efa9e1f2ee14a63451a8dc4fa70f84
Aracelli Doescher <aracellidoescher@outlook.com>
2022-02-14 23:22:40 -0500

    actually implement Mint and Mboolean

--- interpreter.rkt
+++ interpreter.rkt
@@ -16,6 +16,55 @@
   (lambda (tree)
     tree))
 
+;;;; Mappings-------------------------------------------------------------
+(define Mint
+  (lambda (x)
+    (cond
+      ((number? x) x)
+      ((null? x) x)
+      ((and (eq? (operator x) '-) (null? (rightoperand x))) (- (Mint (leftoperand x))))
+      ((eq? (operator x) '-) (- (Mint (leftoperand x)) (Mint (rightoperand x))))
+      ((eq? (operator x) '+) (+ (Mint (leftoperand x)) (Mint (rightoperand x))))
+      ((eq? (operator x) '*) (* (Mint (leftoperand x)) (Mint (rightoperand x))))
+      ((eq? (operator x) '/) (quotient (Mint (leftoperand x)) (Mint (rightoperand x))))
+      ((eq? (operator x) '%) (remainder (Mint (leftoperand x)) (Mint (rightoperand x))))
+      (else (error 'badop "Bad operator")))))
+
+(define Mboolean
+  (lambda (exp)
+    (cond
+      ((boolean? exp) exp)
+      ((null? exp) exp)
+      ((eq? (operator exp) '==) (eq? (Mint (leftoperand exp)) (Mint (rightoperand exp))))
+      ((eq? (operator exp) '!=) (not (eq? (Mint (leftoperand exp)) (Mint (rightoperand exp)))))
+      ((eq? (operator exp) '<) (< (Mint (leftoperand exp)) (Mint (rightoperand exp))))
+      ((eq? (operator exp) '>) (> (Mint (leftoperand exp)) (Mint (rightoperand exp))))
+      ((eq? (operator exp) '<=) (<= (Mint (leftoperand exp)) (Mint (rightoperand exp))))
+      ((eq? (operator exp) '>=) (>= (Mint (leftoperand exp)) (Mint (rightoperand exp))))
+      ((eq? (operator exp) '&&) (and (Mboolean (leftoperand exp)) (Mboolean (rightoperand exp))))
+      ((eq? (operator exp) '||) (or (Mboolean (leftoperand exp)) (Mboolean (rightoperand exp))))
+      ((eq? (operator exp) '!) (not (Mboolean (leftoperand exp))))
+      (else (error 'badop "Bad operator")))))
+
+;;;; Abstractions----------------------------------------------------------
+(define operator
+  (lambda (exp)
+    (if (null? exp)
+        '()
+        (car exp))))
+
+(define leftoperand
+  (lambda (exp)
+    (if (null? (cdr exp))
+        '()
+        (cadr exp))))
+
+(define rightoperand
+  (lambda (exp)
+    (if (null? (cdr (cdr exp)))
+        '()
+        (caddr exp))))
+
 ;;;; Helper Functions--------------------------------------------------
 
 ;; More functions
