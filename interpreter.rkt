391149b353bb768bb497ec1e580b39c9aa3044a3
Aracelli Doescher <aracellidoescher@outlook.com>
2022-04-06 03:30:04 -0400

    implement function calls - mostly working, issues with try-catch-finally

--- interpreter.rkt
+++ interpreter.rkt
@@ -21,14 +21,14 @@
 ;; evaluate searches a parse tree and returns the return value
 (define evaluate
   (lambda (tree state break continue return throw)
-          (returnvalue (Mstate (findmain tree) (setglobals tree state)
+          (returnvalue (Mstate (findmain tree) (setglobals tree state throw)
                        (newnextlambda tree break continue return throw) break continue return throw)))) ; (evaluate (restof tree) 
 
 ;;;; Mappings-------------------------------------------------------------
 
 ;; Mvalue takes an expression and finds the value of that expression
 (define Mvalue
-  (lambda (exp state)
+  (lambda (exp state throw)
     (cond
       [(number? exp) exp]
       [(eq? 'true exp) #t]
@@ -37,43 +37,49 @@
             (valueof exp (vars-list-all state) (values-list-all state))] ; checks if expression is a variable
       [(not (list? exp)) (error 'novar "Variable not declared")]
       [(null? exp) exp]
+      ; mathematical operators
       [(and (eq? (operator exp) '-) (null? (rightoperand exp)))
-            (- (Mvalue (leftoperand exp) state))] ; unary minus
-      [(eq? (operator exp) '-) (- (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '+) (+ (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '*) (* (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state ))]
-      [(eq? (operator exp) '/) (quotient (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '%) (remainder (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '==) (eq? (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '!=) (not (eq? (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state)))]
-      [(eq? (operator exp) '<) (< (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '>) (> (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '<=) (<= (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(eq? (operator exp) '>=) (>= (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(and (eq? (operator exp) '&&) (boolean? (Mvalue (leftoperand exp) state))
-            (boolean? (Mvalue (rightoperand exp) state)))
-       (and (Mvalue (leftoperand exp) state)
-            (Mvalue (rightoperand exp) state))]
-      [(and (eq? (operator exp) '||) (boolean? (Mvalue (leftoperand exp) state))
-            (boolean? (Mvalue (rightoperand exp) state)))
-       (or (Mvalue (leftoperand exp) state)
-           (Mvalue (rightoperand exp) state))]
-      [(and (eq? (operator exp) '!) (boolean? (Mvalue (leftoperand exp) state)))
-       (not (Mvalue (leftoperand exp) state))]
+            (- (Mvalue (leftoperand exp) state throw))] ; unary minus
+      [(eq? (operator exp) '-) (- (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '+) (+ (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '*) (* (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '/) (quotient (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '%) (remainder (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      ; comparison operators
+      [(eq? (operator exp) '==) (eq? (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '!=) (not (eq? (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw)))]
+      [(eq? (operator exp) '<) (< (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '>) (> (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '<=) (<= (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(eq? (operator exp) '>=) (>= (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      ; boolean operators
+      [(and (eq? (operator exp) '&&) (boolean? (Mvalue (leftoperand exp) state throw))
+            (boolean? (Mvalue (rightoperand exp) state throw)))
+       (and (Mvalue (leftoperand exp) state throw)
+            (Mvalue (rightoperand exp) state throw))]
+      [(and (eq? (operator exp) '||) (boolean? (Mvalue (leftoperand exp) state throw))
+            (boolean? (Mvalue (rightoperand exp) state throw)))
+       (or (Mvalue (leftoperand exp) state throw)
+           (Mvalue (rightoperand exp) state throw))]
+      [(and (eq? (operator exp) '!) (boolean? (Mvalue (leftoperand exp) state throw)))
+       (not (Mvalue (leftoperand exp) state throw))]
+      ; assign
       [(eq? (operator exp) '=) (valueof (leftoperand exp) (vars-list (assign (varname exp)
-            (Mvalue (val exp) state) state))
+            (Mvalue (val exp) state) state throw))
             (values-list state))] ; returns the value that was assigned to the specified variable
+      ; function calls
+      [(eq? (operator exp) 'funcall) (Mvalue (returnvalue (callfunctionvalue (functionname exp) (getclosure (functionname exp) (vars-list-all state) (values-list-all state)) (actualparams exp) state throw)) state throw)]
       [else (error 'badexp "Bad expression")])))
 
 ;; Mstate takes an expression and modifies the state accordingly
@@ -86,17 +92,17 @@
       [(and (not (list? exp)) (var? exp (vars-list-all state))) state] ; checks if expression is a variable
       [(not (list? exp)) (error 'novar "Variable not declared")]
       [(null? exp) (next state)]
-      [(and (eq? (operator exp) 'var) (null? (Mvalue (val exp) state)))
+      [(and (eq? (operator exp) 'var) (null? (Mvalue (val exp) state throw)))
             (next (declare (varname exp) state))] ; no value specified (only varname)
       [(eq? (operator exp) 'var) (next (assign (varname exp)
-            (Mvalue (val exp) state) (declare (varname exp) state)))]
+            (Mvalue (val exp) state throw) (declare (varname exp) state)))]
       [(eq? (operator exp) '=) (next (assign (varname exp)
-            (Mvalue (val exp) state) state))]
+            (Mvalue (val exp) state throw) state))]
       ; block
       [(eq? (operator exp) 'begin) (Mstate (firstexp (restof exp)) (addnewlayer state)
             (blocknextlambda (restof exp) next break continue return throw) (newblockbreaklambda break) (newblockcontinuelambda continue) return throw)]
       ; if
-      [(and (eq? (operator exp) 'if) (Mvalue (condition exp) state))
+      [(and (eq? (operator exp) 'if) (Mvalue (condition exp) state throw))
        (Mstate (then exp) (Mstate (condition exp) state next break continue return throw) next break continue return throw)]
       [(and (eq? (operator exp) 'if) (null? (else-statement exp))) (next state)]
       [(eq? (operator exp) 'if) (Mstate (else-statement exp)
@@ -119,7 +125,7 @@
       ; return
       [(eq? (operator exp) 'return) (return (returnstate (returnexp exp) state next break continue return throw))]
       ; throw
-      [(eq? (operator exp) 'throw) (throw state (throwval exp))]
+      [(eq? (operator exp) 'throw) (throw state (Mvalue (throwval exp) state throw))]
       ; values
       [(and (eq? (operator exp) '-) (null? (rightoperand exp))) state] ; unary minus
       [(eq? (operator exp) '-) state]
@@ -133,14 +139,18 @@
       [(eq? (operator exp) '>) state]
       [(eq? (operator exp) '<=) state]
       [(eq? (operator exp) '>=) state]
-      [(and (eq? (operator exp) '&&) (boolean? (Mvalue (leftoperand exp) state))
-            (boolean? (Mvalue (rightoperand exp) state)))
+      [(and (eq? (operator exp) '&&) (boolean? (Mvalue (leftoperand exp) state throw))
+            (boolean? (Mvalue (rightoperand exp) state throw)))
        state]
-      [(and (eq? (operator exp) '||) (boolean? (Mvalue (leftoperand exp) state))
-            (boolean? (Mvalue (rightoperand exp) state)))
+      [(and (eq? (operator exp) '||) (boolean? (Mvalue (leftoperand exp) state throw))
+            (boolean? (Mvalue (rightoperand exp) state throw)))
        state]
-      [(and (eq? (operator exp) '!) (boolean? (Mvalue (leftoperand exp) state)))
+      [(and (eq? (operator exp) '!) (boolean? (Mvalue (leftoperand exp) state throw)))
        state]
+      [(eq? 'function (operator exp)) (next (assign (functionname exp)
+       (makeclosure (formalparams exp) (funcbody exp)) (declare (functionname exp) state)))]
+      ; function calls
+      [(eq? (operator exp) 'funcall) (callfunctionstate (functionname exp) (getclosure (functionname exp) (vars-list-all state) (values-list-all state)) (actualparams exp) state next throw)]
       [else (error 'badstate "Bad state")])))
 
 ;;;; Helper Functions--------------------------------------------------
@@ -159,7 +169,16 @@
   (lambda (var val state)
     (if (var? var (vars-list-all state))
         (newassignstate var val state)
-        (error 'badassign "Bad assignment"))))
+        (error 'badassign "Variable not declared"))))
+
+(define getclosure
+  (lambda (funcname vars-list values-list)
+    (cond
+      [(null? funcname) (error 'badexp "No function name specified")]
+      [(or (null? vars-list) (null? values-list)) (error 'nofunc "Function not defined")]
+      [(and (eq? (first-variable vars-list) funcname) (null? (unbox (first-value values-list)))) (error 'nofunc "Function not defined")]
+      [(eq? (first-variable vars-list) funcname) (unbox (first-value values-list))]
+      [else (getclosure funcname (restof vars-list) (restof values-list))])))
       
 ;; var? searches through the given vars-list and returns #t if the variable has been declared
 (define var?
@@ -193,35 +212,112 @@
 
 ;; setglobals
 (define setglobals
-  (lambda (tree state)
+  (lambda (tree state throw)
     (cond
       [(null? tree) state]
-      [(and (eq? (operator (firstexp tree)) 'var) (null? (Mvalue (val (firstexp tree)) state)))
-            (setglobals (restof tree) (declare (varname (firstexp tree) state)))] ; no value specified (only varname)
+      [(and (eq? (operator (firstexp tree)) 'var) (null? (Mvalue (val (firstexp tree)) state throw)))
+            (setglobals (restof tree) (declare (varname (firstexp tree) state)) throw)] ; no value specified (only varname)
       [(eq? (operator (firstexp tree)) 'var) (setglobals (restof tree) (assign (varname (firstexp tree))
-            (Mvalue (val (firstexp tree)) state) (declare (varname exp) state)))]
-      [(eq? 'function (operator (firstexp tree))) state]
+            (Mvalue (val (firstexp tree)) state throw) (declare (varname (firstexp tree)) state)) throw)]
+      [(eq? 'function (operator (firstexp tree))) (setglobals (restof tree) (assign (functionname (firstexp tree))
+       (makeclosure (formalparams (firstexp tree)) (funcbody (firstexp tree))) (declare (functionname (firstexp tree)) state)) throw)]
       [else error 'badexp "Invalid operation before main"])))
+
+;; makeclosure
+(define makeclosure
+  (lambda (formalparams body)
+    (list formalparams body (lambda (fn vars vals) (funcstate fn vars vals))))) ; what is the current state at time of function call? Fix third element of closure?
 
 ;; findmain
 (define findmain
   (lambda (tree)
     (cond
       [(null? tree) error 'nomain "No main function"]
-      [(eq? 'main (functionname (firstexp tree))) (cons 'begin (funcbody (firstexp tree)))]
+      [(eq? 'main (functionname (firstexp tree))) (funcbody (firstexp tree))]
       [else (findmain (restof tree))])))
 
+;; funcstate returns the portion of the state that's in scope at the time of the function call
+(define funcstate
+  (lambda (funcname vars-list values-list)
+    (cond
+      [(null? vars-list) (error 'funcnotdefined "Function called that does not exist")]
+      [(eq? (first-variable vars-list) funcname) (returnfuncstate vars-list values-list)]
+      [else (funcstate funcname (restof vars-list) (restof values-list))]))) ; this might cause issues? let's find out
+
+;; callfunctionvalue
+(define callfunctionvalue
+  (lambda (funcname closure actualparams state throw)
+    (Mstate (closurebody closure) (bindparams (closurefp closure) actualparams (addnewlayer (closurestate closure funcname state)) state throw)
+     (lambda (s) (error 'noreturn "no return statement"))
+     (newbreaklambda)
+     (newcontinuelambda)
+     (newreturnlambda)
+     (lambda (s e) (throw state e)))))
+
+;; callfunctionstate
+(define callfunctionstate
+  (lambda (funcname closure actualparams state next throw)
+    (Mstate (closurebody closure) (bindparams (closurefp closure) actualparams (addnewlayer (closurestate closure funcname state)) state throw)
+     (lambda (s) (next state))
+     (newbreaklambda)
+     (newcontinuelambda)
+     (funcstatereturnlambda state next)
+     (lambda (s e) (throw state e)))))
+
+;; bindparams
+(define bindparams
+  (lambda (fp ap fstate state throw)
+    (cond
+      [(and (null? fp) (null? ap)) fstate]
+      [(or (null? fp) (null? ap)) (error 'mismatcharguments "The number of arguments expected did not match the number of arguments given")]
+      [else (bindparams (restof fp) (restof ap) (assign (first-param fp) (Mvalue (first-param ap) state throw) (declare (first-param fp) fstate)) state throw)])))
+
 ;;;; Abstractions----------------------------------------------------------
+
+;; funcstatereturnlambda
+(define funcstatereturnlambda
+  (lambda (state next)
+    (lambda (v) (next state))))
 
 ;; functionname finds the name of a function
 (define functionname
   (lambda (exp)
     (cadr exp)))
 
+;; actualparams
+(define actualparams
+  (lambda (exp)
+    (cddr exp)))
+
+;; formalparams
+(define formalparams
+  (lambda (exp)
+    (caddr exp)))
+
 ;; funcbody finds the body of a function
 (define funcbody
   (lambda (exp)
-    (cadddr exp)))
+    (cons 'begin (cadddr exp))))
+
+;; returnfuncstate
+(define returnfuncstate
+  (lambda (vars-list values-list)
+    (cons (list vars-list values-list) '())))
+
+;; closurefp
+(define closurefp
+  (lambda (closure)
+    (car closure)))
+
+;; closurebody
+(define closurebody
+  (lambda (closure)
+    (cadr closure)))
+
+;; closurestate
+(define closurestate
+  (lambda (closure funcname state)
+    ((caddr closure) funcname (vars-list-all state) (values-list-all state))))
 
 ;; newnextlambda returns a base lambda function for the next continuation
 (define newnextlambda
@@ -240,7 +336,7 @@
 ;; newcontinuelambda returns a base lambda function for the continue continuation
 (define newcontinuelambda
   (lambda ()
-    (lambda (s) (error 'noloop "Break cannot be run outside of a loop"))))
+    (lambda (s) (error 'noloop "Continue cannot be run outside of a loop"))))
 
 ;; newreturnlambda returns a base lambda function for the return continuation
 (define newreturnlambda
@@ -316,7 +412,7 @@
   (lambda (exp break continue return throw)
     (lambda (s)
       (if (null? (finallyblock exp))
-          s
+          (error 'noloop "Break cannot be run outside of a loop")
           (Mstate (finallyblock exp) s break break continue return throw)))))
 
 ;; trycontinue returns the lambda function used for the continue continuation when inside a try block
@@ -324,7 +420,7 @@
   (lambda (exp break continue return throw)
     (lambda (s)
        (if (null? (finallyblock exp))
-           s
+           (error 'noloop "Continue cannot be run outside of a loop")
            (Mstate (finallyblock exp) s continue break continue return throw)))))
 
 ;; tryreturn returns the lambda function used for the return continuation when inside a try block
@@ -333,7 +429,7 @@
     (lambda (v)
       (if (null? (finallyblock exp))
           v
-          (Mstate (finallyblock exp) v next break continue return throw)))))
+          (Mstate (finallyblock exp) v return break continue return throw)))))
 
 ;; trythrow returns the lambda function used for the throw continuation when inside a try block
 (define trythrow
@@ -347,11 +443,11 @@
               (lambda (s1) (throw s1 e)) (lambda (s1) (throw s1 e)) (lambda (s1) (throw s1 e))
               (lambda (v) (throw s e)) throw)]
         [(null? (finallyblock exp))
-         (Mstate (catchblock exp) (assign (catchvar exp) e
+         (Mstate (catchblock exp) (assign (catchvar exp) (Mvalue e s throw)
          (declare (catchvar exp) s)) (trynext exp next break continue return throw)
          (trybreak exp break continue return throw) (trycontinue exp break continue return throw)
          (tryreturn exp next break continue return throw) throw)]
-        [else (Mstate (catchblock exp) (assign (catchvar exp) e
+        [else (Mstate (catchblock exp) (assign (catchvar exp) (Mvalue e s throw)
               (declare (catchvar exp) s)) (trynext exp next break continue return throw)
               (trybreak exp break continue return throw) (trycontinue exp break continue return throw)
               (tryreturn exp next break continue return throw)
@@ -391,7 +487,7 @@
 ;; loop runs each iteration of a while loop
 (define loop
   (lambda (exp state next break continue return throw)
-    (if (Mvalue (condition exp) state)
+    (if (Mvalue (condition exp) state throw)
         (Mstate (body exp) state (lambda (s) (Mstate exp s next break continue return throw)) break continue return throw)
         (next state))))
 
@@ -421,7 +517,7 @@
 ;; returnstate returns the state with an added return component
 (define returnstate
   (lambda (exp state next break continue return throw)
-    (cons state (cons (Mvalue exp state) '()))))
+    (cons state (cons (Mvalue exp state throw) '()))))
 
 ;; operator finds the operator of an expression
 (define operator
@@ -551,6 +647,13 @@
     (if (null? vars-list)
         '()
         (car vars-list))))
+
+;; first-param
+(define first-param
+  (lambda (p)
+    (if (null? vars-list)
+        '()
+        (car p))))
 
 ;; values-list finds the list of values in the current layer of the state
 (define values-list
